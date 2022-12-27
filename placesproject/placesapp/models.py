3e2d36c83d47525b00d2d853ccb3714b05cfc4bd
StudentA128 <aleksej.sursyakov.20@gmail.com>
2022-12-27 20:01:47 +0700

    Add model for database

--- placesproject/placesapp/models.py
+++ placesproject/placesapp/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+
+class Places(models.Model):
+    place_title = models.CharField("Название", max_length=64)
+    place_description = models.TextField("Комментарий")
+    place_latitude = models.FloatField("Широта", default=56.0)
+    place_longitude = models.FloatField("Долгота", default=92.88)
+    user_id = models.CharField(max_length=64)
